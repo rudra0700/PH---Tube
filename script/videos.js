44c54b58391aae9b6a19747ab872b070b1e3cc90
rudra0700 <baruarudra7@gmail.com>
2024-10-04 23:08:51 +0600

    feat: search functionality added

--- script/videos.js
+++ script/videos.js
@@ -1,5 +1,5 @@
-const loadVideos = () =>{
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideos = (searchText = "") =>{
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((response) => response.json())
     .then(data => displayVideos(data.videos))
     .catch(error => console.error(error))
@@ -16,33 +16,10 @@ const getTimeString = (time) =>{
     
 }
 
-const cardDemo = {
-    
-        "category_id": "1001",
-        "video_id": "aaaa",
-        "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
-        "title": "Shape of You",
-        "authors": [
-            {
-                "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
-                "profile_name": "Olivia Mitchell",
-                "verified": ""
-            }
-        ],
-        "others": {
-            "views": "100K",
-            "posted_date": "16278"
-        },
-        "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-    
-}
-
 const loadDetails = async (videoId) =>{
   const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
-  
   displayDetails(data.video)
 }
 
@@ -74,9 +51,7 @@ const displayVideos = (videos) =>{
     }else{
         videoContainer.classList.add("grid");
     }
- videos.forEach(video =>{
-    // console.log(video);
-    
+ videos.forEach(video =>{  
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
@@ -109,5 +84,9 @@ const displayVideos = (videos) =>{
  })
   
 }
+
+document.getElementById("search-input").addEventListener("keyup", (event) =>{
+    loadVideos(event.target.value)
+})
 
 loadVideos()
